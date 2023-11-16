1bd8baad7151ea3bc1f7caea53e23a1867ffc6da
mimoun zouhri <zouhrimimoun21@gmail.com>
2023-11-16 17:03:54 +0100

    exe2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
+# config/routes.rb
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :lists do
+    resources :bookmarks, only: [:new, :create, :index, :destroy, :show]
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :bookmarks, only: [:show, :destroy]
+
+  resources :movies
+
+  root "lists#index"
 end
